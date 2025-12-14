4b70ee195027681ca39fe094103f24bdd4c0baf9
Mushthafa Sinuhaji <mi.sinuhaji@gmail.com>
2025-12-13 21:47:02 -0500

    tensor ready wait

--- model.js
+++ model.js
@@ -1,3 +1,10 @@
 import * as tf from 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest';
 
-console.log(tf)
+async function main() {
+  await tf.ready(); // ensure TF.js is fully loaded
+  const x = tf.tensor([1, 2, 3]);
+  x.print();
+  console.log(x);
+}
+
+main();
